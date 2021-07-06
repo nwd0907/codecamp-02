a30b08c7a7cd3efcdf6da7844bef0c98f42cf328
NOHWONDOO <studycloudera01@gmail.com>
2021-07-06 09:43:46 +0900

    Update BoardNewPage

--- class/pages/index.js
+++ class/pages/index.js
@@ -1,11 +1,10 @@
 // import Head from 'next/head'
 // import Image from 'next/image'
 // import styles from '../styles/Home.module.css'
+import { useState } from 'react'
 import {Title, Name, Wrapper, MyInput} from '../styles/Home.styles'
 
 export default function AAA() {
-
-
   return (
     <Wrapper>
       <Title>로그인</Title>
