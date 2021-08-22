461a208fe54fd3de0ae352786a1f9dc6720cd239
nwd0907 <nwd0907>
2021-08-23 03:50:18 +0900

    Update build-test

--- class_build/pages/board/index.js
+++ class_build/pages/board/index.js
@@ -1,8 +1,16 @@
-import { useRouter } from "next/dist/client/router"
+import Head from "next/head";
+import { useRouter } from "next/dist/client/router";
 
-export default function BoardPage(){
+export default function BoardPage() {
+  const router = useRouter();
 
-    const router = useRouter()
-
-    return <button onClick={() => router.push('/market')}>마켓으로 이동</button>
+  return (
+    <>
+      <Head>
+        <meta property="og:title" content="여기는 보드" />
+        <meta property="og:description" content="여기는 보드설명" />
+      </Head>
+      <button onClick={() => router.push("/market")}>마켓으로 이동</button>
+    </>
+  );
 }
--- class_build/pages/market/index.js
+++ class_build/pages/market/index.js
@@ -1,8 +1,16 @@
-import { useRouter } from "next/dist/client/router"
+import Head from "next/head";
+import { useRouter } from "next/dist/client/router";
 
-export default function BoardPage(){
+export default function BoardPage() {
+  const router = useRouter();
 
-    const router = useRouter()
-
-    return <button onClick={() => router.push('/board')}>보드로 이동</button>
+  return (
+    <>
+      <Head>
+        <meta property="og:title" content="여기는 마켓" />
+        <meta property="og:description" content="여기는 마켓설명" />
+      </Head>
+      <button onClick={() => router.push("/board")}>보드로 이동</button>
+    </>
+  );
 }
