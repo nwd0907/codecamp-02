58b0acb2df126dc286166014e78771fac6d39ea7
nwd0907 <nwd0907>
2021-08-24 11:02:08 +0900

    Update OG for SSG

--- class_build/pages/board/index.js
+++ class_build/pages/board/index.js
@@ -4,10 +4,14 @@ export default function BoardPage() {
   return (
     <>
       <Head>
-        <meta property="og:title" content="여기는 보드" />
-        <meta property="og:description" content="여기는 보드설명" />
+        <meta property="og:title" content="정적 게시판!!" />
+        <meta property="og:description" content="이 주소는 정적 주소입니다." />
+        <meta
+          property="og:image"
+          content="https://dullyshin.github.io/2018/08/30/HTML-imgLink/#lg=1&slide=0"
+        />
       </Head>
-      보드페이지
+      <div>여기는 게시판 입니다.</div>
     </>
   );
 }
